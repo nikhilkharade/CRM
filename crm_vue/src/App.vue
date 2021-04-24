a1a5c582286f304174047a7b154b3edf43f3a2e1
nikhilkharade <nikhilkharade070898@gmail.com>
2021-04-24 19:16:44 +0530

    Boiler Plate Front End is done. Need to setup Backend now

--- crm_vue/src/App.vue
+++ crm_vue/src/App.vue
@@ -8,10 +8,19 @@
 </template>
 <script>
   import Navbar from '@/components/layout/Navbar'
+  import axios from 'axios'
   export default {
     name : "App" ,
     components: {
       Navbar
+    },
+    beforeCreate() {
+      this.$store.commit('initializeStore')
+      if(this.$store.state.token) {
+        axios.defaults.headers.common['Authorization'] = "Token" + this.$store.state.token
+      }else{
+        axios.defaults.headers.common['Authorization'] = ""
+      }
     }
     }
 </script>
--- crm_vue/src/router/index.js
+++ crm_vue/src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
 import Signup from '../views/SignUp.vue'
 import SignIn from '../views/SignIn.vue'
+import DashBoard from '../views/dashboard/DashBoard.vue'
+import MyAccount from '../views/dashboard/MyAccount.vue'
+
 
 const routes = [
   {
@@ -26,7 +30,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/dashboard',
+    name: 'DashBoard',
+    component: DashBoard,
+    meta : {
+      requireLogin : true
+    }
+  },
+  {
+    path: '/myAccount',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta : {
+      requireLogin : true
+    }
+  },
 ]
 
 const router = createRouter({
@@ -34,4 +54,11 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to , from , next) => {
+  if(to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/sign-in')
+  } else {
+    next()
+  }
+} )
 export default router
