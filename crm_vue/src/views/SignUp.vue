2ed26758219cefc259be55e4033431516626555b
nikhilkharade <nikhilkharade070898@gmail.com>
2021-04-25 14:57:28 +0530

    SignUp functionality done

--- crm_vue/src/views/SignUp.vue
+++ crm_vue/src/views/SignUp.vue
@@ -3,24 +3,27 @@
         <div class ="columns">
             <div class = "column is-4 is-offset-4">
                 <h1 class="title">Sign Up</h1>
-                <form>
+                <form @submit.prevent="submitForm">
                     <div class="field">
                         <label>Email</label>
                         <div class="control">
-                            <input type="email" name="email" class="input">
+                            <input type="email" name="email" class="input" v-model="username">
                         </div>
                     </div>
                     <div class="field">
                         <label>Password</label>
                         <div class="control">
-                            <input type="password" name="password1" class="input">
+                            <input type="password" name="password1" class="input" v-model="password1">
                         </div>
                     </div>
                     <div class="field">
                         <label>Repeat Password</label>
                         <div class="control">
-                            <input type="password" name="password2" class="input">
+                            <input type="password" name="password2" class="input" v-model="password2">
                         </div>
+                    </div>
+                    <div class = "notification is-danger" v-if="errors.length">
+                        <p v-for="error in errors" v-bind:key="error">{{error}} </p>
                     </div>
                     <div class="field">
                         <div class="control">
@@ -35,7 +38,61 @@
 
 
 <script>
+    import axios from 'axios';
+    import {toast} from 'bulma-toast';
     export default {
-        name : 'Signup'
+        name : 'Signup',
+        data() {
+            return {
+                username : '',
+                password2 : '',
+                password1 : '',
+                errors : []
+            }
+        },
+        methods: {
+            submitForm(){
+                this.errors = []
+                if (this.username == ''){
+                    this.errors.push("The Username is missing")
+                }
+                if (this.password1 == "" || this.password1.length < 4) {
+                    this.errors.push("Password cannot be short or empty")
+                }
+                if (this.password2 !== this.password1) {
+                    this.errors.push("Passwords Do Not Match")
+                }
+                if(!this.errors.length) {
+                    const formData = {
+                        username : this.username,
+                        password : this.password1
+                    }
+                    axios
+                    .post('/api/v1/users/' , formData)
+                    .then(response => {
+                    toast({
+                        message : "Successfully Signed Up",
+                        type : "is-success",
+                        dismissible : true,
+                        pauseOnHover : true,
+                        duration : 2000,
+                        position : 'bottom-right'
+                    })
+
+                    this.$router.push('/sign-in')
+                    })
+                    .catch(error => {
+                        if(error.response) {
+                            for(const property in error.response.data) {
+                                this.errors.push(`${property} : ${error.response.data[property]}`)
+                            }
+                        } else if(error.message) {
+                            this.errors.push("Something went wrong please try again")
+                        }
+                    })
+                }
+            }
+        }
+        
         }
 </script>
