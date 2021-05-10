4c1bc54363d636c437ec588b1a415f417dd6d86f
nikhilkharade <nikhilkharade070898@gmail.com>
2021-05-11 00:07:11 +0530

    App setup is done

---
+++ crm_django/team/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(User , related_name='teams')
+    created_by = models.ForeignKey(User , related_name='created_teams' , on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
